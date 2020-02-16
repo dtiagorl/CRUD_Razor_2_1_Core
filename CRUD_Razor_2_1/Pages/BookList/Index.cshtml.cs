ceedad98852e5a6e9a6c60246fcf8d2b2a378721
Tiago R. Ladeia <dtiagorl@gmail.com>
2020-02-16 18:08:10 -0300

    Add Create Page

--- CRUD_Razor_2_1/Pages/BookList/Index.cshtml.cs
+++ CRUD_Razor_2_1/Pages/BookList/Index.cshtml.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRUD_Razor_2_1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_Razor_2_1
 {
     public class IndexModel : PageModel
     {
-        public string someData { get; set; }
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Book> Books { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
         {
-            someData = "This is the first Property!";
+            _db = db;
+        }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
         }
     }
 }
